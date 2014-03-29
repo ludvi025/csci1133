02163f6952d8b837d1428d929efa5e797faf2ef4
Adam Ludvik <ludvi025@umn.edu>
2014-03-28 20:18:10 -0500

    Only recursively unzips the file passed by the command line.

--- runzip.py
+++ runzip.py
@@ -3,7 +3,13 @@
 # Unzips all files in the directory its run and then unzips all zipped
 # files in files just unzipped.
 
-import zipfile as z, os
+import zipfile as z, os, argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("zip_file")
+    zipfile = (parser.parse_args()).zip_file
+    unzip(zipfile)
 
 def getOutputDir(file_name):
     return '.'.join(file_name.split('.')[0:-1])
@@ -24,5 +30,14 @@ def unzipAll(path=os.getcwd()):
         if os.path.isdir(f):
             unzipAll(os.path.join(path,f))
 
+def unzip(path):
+    if z.is_zipfile(path):
+        unzipped_name = getOutputDir(path)
+        z.ZipFile(path).extractall(unzipped_name)
+        os.remove(path)
+
+        for filename in os.listdir(unzipped_name):
+            unzip(filename)
+
 if __name__ == "__main__":
-    unzipAll()
+    main()
