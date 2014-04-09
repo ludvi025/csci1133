220ac8e08fae97814145357210cd7ccf1d03e620
Adam Ludvik <ludvi025@umn.edu>
2014-04-09 09:25:03 -0500

    Fixed last name, first name swapped.

--- sub_parser.py
+++ sub_parser.py
@@ -1,6 +1,6 @@
 import re
 
-regex = r"(?:.*/)?(?P<lastname>[^/]*) (?P<firstname>[^/]*)_(?P<moodleid>\d*)_\w*_\w*_\w*/?.*"
+regex = r"(?:.*/)?(?P<firstname>[^/]*) (?P<lastname>[^/]*)_(?P<moodleid>\d*)_\w*_\w*_\w*/?.*"
 #compileing the regex is optional. It is a performance optimization.
 cmp_regex = re.compile(regex)
 
@@ -16,11 +16,11 @@ def parse(file_name):
       return None
 
 def test():
-    test1 = "lastname firstname_1128269_assignsubmission_file_HW2/"
-    test2 = "lastname(alternatelastname) firstname_1093811_assignsubmission_file_HW2/"
-    test3 = "stuff/directories/lastname firstname_1128269_assignsubmission_file_HW2/"
-    test4 = "stuff/directories/lastname firstname_1128269_assignsubmission_file_HW2/stuffagain/file.py"
-    test5 = "stuff/directories/lastname(alternatelastname) firstname_1093811_assignsubmission_file_HW2/moredire/file.py"
+    test1 = "firstname lastname_1128269_assignsubmission_file_HW2/"
+    test2 = "firstname(alternatefirstname) lastname_1093811_assignsubmission_file_HW2/"
+    test3 = "stuff/directories/firstname lastname_1128269_assignsubmission_file_HW2/"
+    test4 = "stuff/directories/firstname lastname_1128269_assignsubmission_file_HW2/stuffagain/file.py"
+    test5 = "stuff/directories/firstname(alternatefirstname) lastname_1093811_assignsubmission_file_HW2/moredire/file.py"
 
     for test in [test1, test2, test3, test4, test5]:
-        print(getName(test))
+        print(parse(test))
