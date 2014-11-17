551ab729746f71ff0a72e319d80adb24200637db
Tom Postler <tom@postler.me>
2014-11-17 10:39:14 -0600

    print_menu_common reformatted.

--- lib/menu/menu_common.py
+++ lib/menu/menu_common.py
@@ -6,6 +6,16 @@ def print_menu_common(dec, name, optenum, options):
     print(dec + name.center(78) + dec)
     print(dec*80)
     for i in range(len(optenum)):
-        print(dec + str(i+1).rjust(3) + ' : ' + options[optenum(i+1)].ljust(72) + dec)
+        first = True
+        for line in options[optenum(i+1)].split('\n'):
+            if first:
+                print(dec + str(i+1).rjust(3) + ' : ' + line.ljust(72) + dec)
+                first = False
+            else:
+                print(dec + ' '*6 + line.ljust(72) + dec)
     print(dec*80)
     print()
+
+
+def print_help_menu_common(name, optenum, options):
+    print_menu_common('?', name, optenum, options)
